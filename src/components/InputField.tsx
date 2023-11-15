b12f0dcf96caaca1b8ab7485b4d6366ef10dd8a5
George <georgemitusov@gmail.com>
2023-11-15 21:42:01 +0100

    First update

---
+++ src/components/InputField.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({ todo, setTodo } : Props )  => {
+  
+  return (
+    <form className='input'>
+      <input 
+        type='input' 
+        placeholder='Enter a task'
+        className='input__box'
+      />
+      <button
+        className='input__submit'
+        type='submit'
+      > Go! </button>
+    </form>
+  )
+}
+
+export default InputField;
