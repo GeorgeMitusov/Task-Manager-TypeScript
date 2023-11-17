3e915d1251c7b29b8b34b7cc91b895644855c412
George <georgemitusov@gmail.com>
2023-11-17 12:36:31 +0100

    making dragndrop, prepared components for it

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,18 +8,59 @@ interface Props {
 }
 
 const TodoList: React.FC<Props> = ({ todos, dispatch }) => {
+
   return (
-    <div className='todos'>
-      { todos.map( todo => (
-          <SingleTodo 
-            key={todo.id} 
-            todo={todo} 
-            dispatch={dispatch} 
-          />
-        ) 
-      )}
+    <div className='container'>
+      <div className="todos">
+        <h6 className="todos__heading"> Active tasks</h6>
+        { todos.map( todo => <SingleTodo key={todo.id} todo={todo} dispatch={dispatch} /> )}
+      </div>
+      <div className="todos remove">
+        <h6 className="todos__heading"> Completed tasks</h6>
+        { todos.map( todo => <SingleTodo key={todo.id} todo={todo} dispatch={dispatch} /> )}
+      </div>
     </div>
   )
+
+  // return (
+  //   <div className='todos'>
+  //     { todos.map( todo => (
+  //         <SingleTodo 
+  //           key={todo.id} 
+  //           todo={todo} 
+  //           dispatch={dispatch} 
+  //         />
+  //       ) 
+  //     )}
+  //   </div>
+  // )
+
 }
 
 export default TodoList;
+
+// import React from 'react';
+// import { Todo } from './model';
+// import SingleTodo from './SingleTodo';
+
+// interface Props {
+//   todos: Todo[],
+//   dispatch: React.Dispatch<any>
+// }
+
+// const TodoList: React.FC<Props> = ({ todos, dispatch }) => {
+//   return (
+//     <div className='todos'>
+//       { todos.map( todo => (
+//           <SingleTodo 
+//             key={todo.id} 
+//             todo={todo} 
+//             dispatch={dispatch} 
+//           />
+//         ) 
+//       )}
+//     </div>
+//   )
+// }
+
+// export default TodoList;
