e8725ea694877c9806858fdba44fe9598348e179
George <georgemitusov@gmail.com>
2023-11-17 16:43:48 +0100

    final version

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import { reducer } from './reducer/reducer';
 import { Todo } from './components/model';
 import TodoList from './components/TodoList';
 import { ACTIONS } from './actions/actions';
-import { completedReducer } from './reducer/completedReducer';
 
 const App: React.FC = () => {
 
   const [ todos, todosDispatch ] = useReducer(reducer, [] as Todo[]);
-  const [ completedTodos, completedDispatch ] = useReducer(completedReducer, [] as Todo[]);
   const [ todo, setTodo ] = useState<string>('');
 
   function handleAdd() {
@@ -37,43 +35,3 @@ const App: React.FC = () => {
 }
 
 export default App;
-
-// import React, { useReducer, useState } from 'react';
-// import './styles/App.css';
-
-// import InputField from './components/InputField';
-// import { reducer } from './reducer/reducer';
-// import { Todo } from './components/model';
-// import TodoList from './components/TodoList';
-// import { ACTIONS } from './actions/actions';
-// import { completedReducer } from './reducer/completedReducer';
-
-// const App: React.FC = () => {
-
-//   const [ todos, dispatch ] = useReducer(reducer, [] as Todo[]);
-//   const [ completedTodos, dispatch ] = useReducer(completedReducer, [] as Todo[]);
-//   const [ todo, setTodo ] = useState<string>('');
-
-//   function handleAdd() {
-//     dispatch({ type: ACTIONS.ADD_TODO, payload: todo });
-//     setTodo('');
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1 className="app-title"> TASK MANAGER </h1>
-
-//       <div className="app-container">
-
-//         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-
-//         <TodoList todos={todos} dispatch={dispatch} />
-
-//       </div>
-      
-//     </div>
-//   );
-
-// }
-
-// export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -24,29 +24,3 @@ const TodoList: React.FC<Props> = ({ todos, todosDispatch }) => {
 }
 
 export default TodoList;
-
-// import React from 'react';
-// import { Todo } from './model';
-// import SingleTodo from './SingleTodo';
-
-// interface Props {
-//   todos: Todo[],
-//   dispatch: React.Dispatch<any>
-// }
-
-// const TodoList: React.FC<Props> = ({ todos, dispatch }) => {
-//   return (
-//     <div className='todos'>
-//       { todos.map( todo => (
-//           <SingleTodo 
-//             key={todo.id} 
-//             todo={todo} 
-//             dispatch={dispatch} 
-//           />
-//         ) 
-//       )}
-//     </div>
-//   )
-// }
-
-// export default TodoList;
